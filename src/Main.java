a14bea09f01bb6305f20e9b38f2aca03795093eb
El_KRAX <josemamanila51@gmail.com>
2025-07-19 21:01:04 -0300

    version 1.2 (viene acompañado de condicionales a la aplicacion para que sea mas dinamica

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,35 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner lecTecla = new Scanner(System.in);
-        System.out.println("Cuánto dinero posee?        (ingrese numeros) ");
 
-        int dineroDispone = lecTecla.nextInt();
+
+        System.out.println("por donde quiere empezar a registrar, desde la cantidad de productos o la cantidad de dinero");
+        String decide = lecTecla.next();
+
+
+        if (decide.equals("productos")) {
+
+            System.out.println("Muy bien la lista se creara apartir de la cantidad que le asigne");
+            System.out.println("Igrese la cantidad:");
+            int decideNum = lecTecla.next();
+            String[] listaDeProductos = new String[decideNum];
+
+        } else if (decide.equals("dinero")) {
+
+            System.out.println("Cuánto dinero posee para gastar?        (ingrese numeros) ");
+            int dineroDispone = lecTecla.nextInt();
+            if (dineroDispone > 0) {
+                System.out.println("su dinero es " + dineroDispone);
+                String[] listaDeProductos = new String[6];
+
+            } else {
+                System.out.println("ingrese un numero positivo || ingrese un dato valido");
+
+            }
+        }
+
 
         System.out.println("ingrese lo que desea agregar a la lista ");
-
-        String[] listaDeProductos = new String[6];
-        do {
-            System.out.println(" Ingrese el nombre del producto?");
-        }
-        while (dineroDispone > 0);
-            String nombreProducto = lecTecla.next();
-
-
-
-        System.out.println("dinero insuficiente ");
 
     }
 }
