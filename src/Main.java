41fa727b7f4dfa183788562508923ee24328efa4
El_KRAX <josemamanila51@gmail.com>
2025-07-20 21:52:46 -0300

    version 1.3 (esta vez es el mejoramiento de la interface y con mejores preguntas y respuestas)

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -6,6 +6,8 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner lecTecla = new Scanner(System.in);
+        String txt1 = "ingrese el nombre del producto:";
+        String txt2 = "inidique su precio:";
 
 
         System.out.println("por donde quiere empezar a registrar, desde la cantidad de productos o la cantidad de dinero");
@@ -16,8 +18,22 @@ public class Main {
 
             System.out.println("Muy bien la lista se creara apartir de la cantidad que le asigne");
             System.out.println("Igrese la cantidad:");
-            int decideNum = lecTecla.next();
-            String[] listaDeProductos = new String[decideNum];
+            int decideNum = lecTecla.nextInt();
+            int i = 0 ;
+
+            while (i > decideNum);
+            System.out.println(txt1);
+            decide= lecTecla.next();
+            List<String> listaDeProductos = new ArrayList<>(decideNum*2);
+            listaDeProductos.add(decide);
+            System.out.println(txt2);
+            decide = lecTecla.next();
+            listaDeProductos.add(decide);
+
+            System.out.println(listaDeProductos.get(1));
+
+             // logica de agregado de datos a la lsta , y luego cuando quiera terminar o finalizar que se se muestre o que agrego ala lista
+             //para verificar si le falta algun dato para agregar a la lista
 
         } else if (decide.equals("dinero")) {
 
@@ -34,7 +50,6 @@ public class Main {
         }
 
 
-        System.out.println("ingrese lo que desea agregar a la lista ");
 
     }
 }
